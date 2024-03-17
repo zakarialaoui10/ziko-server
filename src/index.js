edd8aa51d0df77c4c761a3e8ac91c0c34769f268
zakarialaoui10 <zakarialaoui10@gmail.com>
2024-03-17 17:46:06 +0000

    fix

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ import Ziko from "ziko";
 import lottie from "lottie-web"
 class ZikoLottiePlayer extends Ziko.ZikoUIElement{
     constructor(src){
-        super("div","div");
+        super("figure","figure");
         this.lottie=lottie.loadAnimation({
             container: this.element,
             renderer: 'svg',
-            loop: true,
-            autoplay: true,
             path: src
           });
+        src instanceof Object ? Object.assign(this.lottie,{animationData:src}) : Object.assign(this.lottie,{src})
+    }
+    play(){
+        this.lottie.play();
+        return this;
+    }
+    pause(){
+        this.lottie.pause();
+        return this;
+    }
+    destroy(){
+        this.lottie.destroy();
+        return this;
     }
 }
 const ZikoLottie=(src)=>new ZikoLottiePlayer(src);
