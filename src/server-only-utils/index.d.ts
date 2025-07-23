8e87db8a0be42baf84d7157072bf1fa7610370af
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-07-23 13:48:52 +0100

    fix renderDomToString Ts declaration

--- src/server-only-utils/index.d.ts
+++ src/server-only-utils/index.d.ts
@@ -1,3 +1,3 @@
 import {ZikoUIElement} from "ziko"
 export declare function renderToString(UIElement: ZikoUIElement): string;
-export declare function renderToDomString(UIElement: HTMLElement): string;
+export declare function renderDomToString(UIElement: HTMLElement): string;
