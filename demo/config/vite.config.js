7195be5d6bc616b2ebac50b0f5f24d9600c46dce
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-09-13 16:13:04 +0100

    .

--- demo/config/vite.config.js
+++ demo/config/vite.config.js
@@ -1,45 +1,8 @@
 import { defineConfig } from "vite";
-import { resolve } from "node:path";
-import {prerender} from "ziko-server/prerender"
+import { build } from "ziko-server/build";
 export default defineConfig(({ command, mode }) =>{
-    const outDir = 'dist'
-    const Target = process.env.TARGET;
-    const isClient = (Target === 'client')
-    const isServer = (Target === 'server')
-    const isStatic = (Target === 'static')
-    if(isClient){
-        return {
-            build : {
-                // base: "./client/",
-                modulePreload: false,
-                outDir : `${outDir}/.client`, 
-                emptyOutDir: true,
-                manifest: true, 
-                rollupOptions: {
-                    input: {
-                     main :  resolve(process.cwd(), "src/entries/entry-client.js"),
-                     index : resolve(process.cwd(), 'index.html')
-                    },
-                    output: {
-                        entryFileNames: `assets/[name].[hash].js`,
-                        chunkFileNames: `assets/[name].[hash].js`,
-                        assetFileNames: `assets/[name].[hash].[ext]`,
-                    },
-                    },
-            }
-        }
-    }
-    if(isServer){
-        return {
-            build : {
-                outDir : `${outDir}/.server`,
-                ssr :  resolve(process.cwd(), "src/entries/entry-server.js"),
-                copyPublicDir: false,
-            }
-        }        
-    }
-    if(isStatic){
-        prerender({outDir : `${outDir}`})   
-        return {}
-    }
+    return build({
+        outDir : 'dist'
+    })
 })
+
---
+++ src/build/index.js
@@ -0,0 +1,36 @@
+import { resolve } from 'path'
+export function build({outDir = 'dist'} = {}){
+    const Target = process.env.TARGET;
+    const isClient = (Target === 'client')
+    const isServer = (Target === 'server')
+    if(isClient){
+        return {
+            build : {
+                modulePreload: false,
+                outDir : `${outDir}/.client`, 
+                emptyOutDir: true,
+                manifest: true, 
+                rollupOptions: {
+                    input: {
+                     main :  resolve(process.cwd(), "src/entries/entry-client.js"),
+                     index : resolve(process.cwd(), 'index.html')
+                    },
+                    output: {
+                        entryFileNames: `assets/[name].[hash].js`,
+                        chunkFileNames: `assets/[name].[hash].js`,
+                        assetFileNames: `assets/[name].[hash].[ext]`,
+                    },
+                    },
+            }
+        }
+    }
+    if(isServer){
+        return {
+            build : {
+                outDir : `${outDir}/.server`,
+                ssr :  resolve(process.cwd(), "src/entries/entry-server.js"),
+                copyPublicDir: false,
+            }
+        }        
+    }
+}
